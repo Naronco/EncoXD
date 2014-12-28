bda51407b7191fc13514035e5408bfe46635ce79
WebFreak001 <janju007@web.de>
2014-12-28 14:21:29 +0100

    Delta bug?

--- source/EncoXD/EncoDesktop/FPSRotation.d
+++ source/EncoXD/EncoDesktop/FPSRotation.d
@@ -17,12 +17,13 @@ class FPSRotation : IComponent
 	}
 	
 	public override void preUpdate(f64 deltaTime) {
+		Logger.writeln();
 	}
 	
 	public override void update(f64 deltaTime)
 	{
 		mstate = Mouse.getState();
-		object.transform.rotation -= vec3(mstate.offset.y, mstate.offset.x, 0) * 0.005f;
+		object.transform.rotation -= vec3(mstate.offset.y, mstate.offset.x, 0) * 0.01;
 		if(object.transform.rotation.x > 1.5707963f) object.transform.rotation.x = 1.5707963f;
 		if(object.transform.rotation.x < -1.5707963f) object.transform.rotation.x = -1.5707963f;
 	}
