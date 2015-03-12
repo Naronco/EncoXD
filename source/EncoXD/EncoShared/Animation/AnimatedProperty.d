43b1a320a8d55b99607b326256ffb95ad2b87c49
WebFreak001 <janju007@web.de>
2015-03-12 17:30:45 +0100

    Added support for non numeric values for animations

--- source/EncoXD/EncoShared/Animation/AnimatedProperty.d
+++ source/EncoXD/EncoShared/Animation/AnimatedProperty.d
@@ -8,6 +8,7 @@ enum AnimationState
 	Still
 }
 
+/// Animation handler for a numeric type
 class AnimatedProperty(T)
 {
 	private T intVal = 0;
@@ -49,7 +50,91 @@ class AnimatedProperty(T)
 	/// Easing function from Animation.easingFunctions
 	public @property string easingType() { return easeType; }
 	/// ditto
-	public @property void easingType(string val) { assert((val.trim().toLower() in Animation.easingFunctions) !is null); easeType = val.trim().toLower(); }
+	public @property void easingType(string val)
+	{
+		assert((val.trim().toLower() in Animation.easingFunctions) !is null);
+		easeType = val.trim().toLower();
+	}
+
+	/// Length in milliseconds
+	public @property int length() { return cast(int)round(1000.0 / iTimeSec); }
+	/// ditto
+	public @property void length(int ms) { iTimeSec = 1000.0 / ms; }
+
+	/// Update
+	/// Params:
+	///		delta = Delta time in seconds
+	public void update(f64 delta)
+	{
+		if(state == AnimationState.Animating)
+		{
+			time += delta * iTimeSec;
+			if(time >= 1)
+			{
+				intVal = finishVal;
+				state = AnimationState.Still;
+				onDone();
+			}
+		}
+	}
+
+
+	public override int opUnary(string s)()
+	{
+		return intVal.opUnary!(s)();
+	}
+
+	public U opCast(U)()
+	{
+		return cast(U)value;
+	}
+
+	public U opBinary(string op)(Object b)
+	{
+		return intVal.opBinary!(op)(b);
+	}
+}
+
+
+/// Animated Property with custom functions and support for more types than numerics
+class AnimatedFunctionValue(T)
+{
+	T function(T, T, f64) interpolationFunction = (T delta, T start, f64 time) { return start + delta * time; };
+
+	private T intVal = 0;
+	private T finishVal = 0;
+	private f64 time = 0;
+	private f64 iTimeSec = 1;
+	private AnimationState state = AnimationState.Still;
+
+	public Trigger onDone = new Trigger();
+
+	public this()
+	{
+	}
+
+	public this(T val)
+	{
+		intVal = val;
+	}
+
+	public @property T value()
+	{
+		if(state == AnimationState.Still) return intVal;
+		return interpolationFunction(finishVal - intVal, intVal, min(1, max(0, time)));
+	}
+
+	public @property void value(T val)
+	{
+		if(finishVal == val) return;
+		intVal = value;
+		finishVal = val;
+		state = AnimationState.Animating;
+		time = 0;
+	}
+
+	/// Current animation state
+	public @property AnimationState animationState() { return state; }
 
 	/// Length in milliseconds
 	public @property int length() { return cast(int)round(1000.0 / iTimeSec); }
