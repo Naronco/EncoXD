94ce6b3f7ac59f6f04d4c6875c5fe7f8c2423d5d
WebFreak001 <janju007@web.de>
2015-06-20 23:54:32 +0200

    Added font property to TextControl

--- source/EncoXD/EncoShared/Enco/Shared/GUI/TextControl.d
+++ source/EncoXD/EncoShared/Enco/Shared/GUI/TextControl.d
@@ -5,7 +5,7 @@ import EncoShared;
 class TextControl(T : ITexture) : Control
 {
 	private ITexture m_texture;
-	private string m_text;
+	private string m_text = "";
 	private Font m_font;
 	private vec4 m_color4f;
 	private Color m_color;
@@ -17,6 +17,23 @@ class TextControl(T : ITexture) : Control
 	public @property void color(Color color)
 	{
 		m_color = color; m_color4f = vec4(color.R * 0.00392156862f, color.G * 0.00392156862, color.B * 0.00392156862, 1);
+	}
+
+	public @property Font font()
+	{
+		return m_font;
+	}
+	public @property void font(Font font)
+	{
+		if(m_font == font)
+			return;
+		m_font = font;
+		if(m_text != "")
+		{
+			m_texture = m_font.render!T(m_text, Color.White);
+			width = m_texture.width;
+			height = m_texture.height;
+		}
 	}
 
 	public @property string text()
@@ -33,7 +50,7 @@ class TextControl(T : ITexture) : Control
 		height = m_texture.height;
 	}
 
-	public this(Font font)
+	public this(Font font = null)
 	{
 		m_font = font;
 		color = Color.White;
